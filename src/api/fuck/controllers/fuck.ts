c39fbdf03786c30447fd331f875762c3d7d426a0
mukesh adhykari <mukesh@adhykari>
2025-07-19 10:41:56 +0900

    remove f

--- src/api/fuck/controllers/fuck.ts
+++
@@ -1,7 +0,0 @@
-/**
- * fuck controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::fuck.fuck');
--- src/api/fuck/routes/fuck.ts
+++
@@ -1,7 +0,0 @@
-/**
- * fuck router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::fuck.fuck');
